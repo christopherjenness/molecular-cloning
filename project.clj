3dd35ffaf92661974d5ac49829a345811c2bd493
Christopher Jenness <christopherjenness@gmail.com>
2017-08-29 06:34:33 -0400

    add cider

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject molecular-cloning "0.1.0-SNAPSHOT"
+(defproject command-line-args "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :profiles {:dev {:plugins [[cider/cider-nrepl "0.7.0"]]}})
