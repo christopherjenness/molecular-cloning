d43e4a0f098bfa1375b389e8d4bda8e876825207
Christopher Jenness <christopherjenness@gmail.com>
2017-09-12 05:15:18 -0400

    Add NEB -> regex converter

--- scrapeNEB.py
+++ scrapeNEB.py
@@ -17,7 +17,28 @@ def import_NEB(url=URL):
                       index=df['Recognition Sequence']).stack()
     df = df.reset_index()[[0, 'Recognition Sequence']]
     df.columns = ['Enzyme', 'Recognition Sequence']
+    df = df[~df['Recognition Sequence'].str.contains("\(")]
+    df = df[df['Recognition Sequence'].str.contains("/")]
     series = pd.Series(df['Enzyme'].values,
                        index=df['Recognition Sequence'])
     restriction_sites = series.to_dict()
     return restriction_sites
+
+
+def convert_to_regex(restriction_site, NEB_dict):
+    enzyme = NEB_dict[restriction_site]
+    regex = restriction_site
+    regex = regex.replace('N', '[ATCG]')
+    regex = regex.replace('Y', '[CT]')
+    regex = regex.replace('M', '[CA]')
+    regex = regex.replace('R', '[AG]')
+    regex = regex.replace('K', '[TC]')
+    regex = regex.replace('S', '[GC]')
+    regex = regex.replace('W', '[AT]')
+    regex = regex.replace('D', '[AGT]')
+    regex = regex.replace('V', '[ACG]')
+    regex = regex.replace('H', '[ACT]')
+    regex = regex.replace('B', '[CGT]')
+    cutsite = regex.index('/')
+    regex = regex.replace('/', '')
+    return regex, enzyme, cutsite
