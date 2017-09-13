9424a309aa689a895b2178aa55f8484925e8bda9
Christopher Jenness <christopherjenness@gmail.com>
2017-09-13 05:13:35 -0400

    Add cutsites dict to settings
    This needs to be locally stored eventually

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
 from collections import defaultdict
+import scrapeNEB
+
 CODONS = {"TTT": "Phe", "TCT": "Ser", "TAT": "Tyr", "TGT": "Cys",
           "TTC": "Phe", "TCC": "Ser", "TAC": "Tyr", "TGC": "Cys",
           "TTA": "Leu", "TCA": "Ser", "TAA": "TER", "TGA": "TER",
@@ -22,8 +24,12 @@ ACIDS = {"Phe": "F", "Ser": "S", "Tyr": "Y", "Cys": "C", "Leu": "L",
          "Val": "V", "Ile": "L", "Met": "M", "Asp": "D", "Thr": "T",
          "Lys": "K"}
 
-
 WEIGHTS = {'A': 89.1, 'R': 174.2, 'N': 132.1, 'D': 133.1, 'C': 121.2,
            'E': 147.1, 'Q': 146.2, 'G': 75.1, 'H': 155.2, 'I': 131.2,
            'L': 131.2, 'K': 146.2, 'M': 149.2, 'F': 165.2, 'P': 115.1,
-           'S': 105.1, 'T': 119.1, 'W': 204.2, 'Y': 181.2, 'V': 117.1}
+           'S': 105.1, 'T': 119.1, 'W': 204.2, 'Y': 181.2, 'V': 117.1,
+           '-': 0.0}
+
+ABSORBANCES = {'W': 5500, 'Y': 1490}
+
+CUTSITES = scrapeNEB.generate_regex_dict()
